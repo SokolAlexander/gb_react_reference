abb33a4909e0d117a4a04c8c05bc8cf1d5a72f99
SokolAlexander <asokol1994@gmail.com>
2021-04-26 16:07:14 +0300

    lesson1: added components, styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+import { Message } from './components/Message';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* инлайновые стили здесь - в демонстрационных целях, лучше использовать css */}
+      <div style={{ marginLeft: '40px' }}>
+        <h1 className="header">Hello React</h1>
+      </div>
+      <Message text="hi! i am a message" />
     </div>
   );
 }
---
+++ src/components/Message/index.js
@@ -0,0 +1,7 @@
+import './styles.css';
+
+export const Message = ({ text }) => (
+  <div className='message'>
+    <span>{text}</span>
+  </div>
+);
